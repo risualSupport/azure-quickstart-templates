8c3f19a6ff12cfd35e941184d1f0a146dd1db28f
risualSupport <30312631+risualSupport@users.noreply.github.com>
2019-04-24 12:54:06 +0100

    Update mysql.sh

--- openedx-scalable-ubuntu/mysql.sh
+++ openedx-scalable-ubuntu/mysql.sh
@@ -4,8 +4,9 @@ echo "Updating mysql configs in /etc/mysql/mysql.conf.d/mysqld.cnf."
 sudo sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
 echo "Updated mysql bind address in /etc/mysql/mysql.conf.d/mysqld.cnf to 0.0.0.0 to allow external connections."
 
-echo "Assigning mysql user user1 access on %."
+echo "Assigning mysql user root + migrate access on %."
 sudo mysql --execute "GRANT ALL PRIVILEGES ON *.* TO root@'%'; FLUSH PRIVILEGES;"
+sudo mysql --execute "GRANT ALL PRIVILEGES ON *.* TO migrate@'%'; FLUSH PRIVILEGES;"
 echo "Assigned mysql user root access on all hosts."
 
 echo "Adding Firewall Rule"
